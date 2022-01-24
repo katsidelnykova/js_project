3de287675ed5bf059f5bca6826d31723a503f758
Kat Sidelnykova <kat.sidelnykova@gmail.com>
2022-01-24 22:41:25 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// let number = 5;
+// number = 10;
+// console.log(number);
+
+const answers = [];
+
+answers [0] = prompt('Как ваше имя?', '');
+answers [1] = prompt('Как вашa фамилия?', '');
+answers [2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
